f2ca525fe2eba9127e850b2dae710135d5931be0
Mathen <23natenathan@gmail.com>
2022-09-30 23:46:44 -0400

    Maze Final

--- Assets/Scripts/LevelScripts/Maze.cs
+++ Assets/Scripts/LevelScripts/Maze.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Maze : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float speed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir += Vector2.down;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<NumberOne>() != null)
+        {
+            GameManager.Instance.NextLevel();
+        }
     }
 }
