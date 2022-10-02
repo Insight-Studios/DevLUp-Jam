7d68cee811b418bf38c015ccd3d337d037081f8a
Mathen <23natenathan@gmail.com>
2022-10-02 14:12:13 -0400

    Quit function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,16 +32,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void Quit()
     {
-
+        Application.Quit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("Exiting game");
+            Quit();
+        }
     }
 
     IEnumerator StartLevel()
